5dab43319b0fa5c573f28955f486c93fc54f9833
faustin <fausma01@gmail.com>
2025-03-03 18:09:50 +0200

    Add Multiple  components in one  project

--- soft-blog/src/App.js
+++ soft-blog/src/App.js
@@ -1,25 +1,15 @@
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title="Welcome to the New  Blog";
-  const likes=50;
-  const person={
-    name:'Faustin',age:'70',address:'Kigali'
-  }
-  const link="www.google.com";
   return (
     <div className="App">
-   <div  className="content">
-    <h1>{title}</h1> 
-    <p>liked {likes} times</p>
-    <p>{person.name}</p>
-    <p>Array :{[1,2,4,5,7]}</p>
-    <p>{Math.random(90)*10}</p>
-   <a href={link}>Google  site</a>
-
-     </div>
-        
+      <Navbar/>
+      <div className="content">
+      <Home/>
+      </div>
     </div>
   );
 }
---
+++ soft-blog/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>The soft blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
