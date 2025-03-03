4ae6e7300386c38dcd0f4c495d379c6787749d78
faustin <fausma01@gmail.com>
2025-03-03 17:46:55 +0200

    Add  dynamic   value in  templates

--- soft-blog/src/App.js
+++ soft-blog/src/App.js
@@ -2,10 +2,22 @@
 import './App.css';
 
 function App() {
+  const title="Welcome to the New  Blog";
+  const likes=50;
+  const person={
+    name:'Faustin',age:'70',address:'Kigali'
+  }
+  const link="www.google.com";
   return (
     <div className="App">
    <div  className="content">
-    <h1>App  compoents</h1> 
+    <h1>{title}</h1> 
+    <p>liked {likes} times</p>
+    <p>{person.name}</p>
+    <p>Array :{[1,2,4,5,7]}</p>
+    <p>{Math.random(90)*10}</p>
+   <a href={link}>Google  site</a>
+
      </div>
         
     </div>
